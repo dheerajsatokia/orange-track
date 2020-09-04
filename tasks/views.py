49da17ec4d1fcb2e92c1a94ad1ed58c5f6fec4ec
Dheeraj Satokia <dheeraj.satokia57@gmail.com>
2020-09-04 12:18:42 +0530

    Removed all unused imports and statements from both Stages and Tasks API

--- tasks/views.py
+++ tasks/views.py
@@ -1,11 +1,8 @@
-# from django.shortcuts import render
 from rest_framework import viewsets
 from . import models
 from . import serializer
-from rest_framework.permissions import IsAuthenticated, AllowAny
+from rest_framework.permissions import IsAuthenticated
 
-
-# Create your views here.
 
 class TasksViewSet(viewsets.ModelViewSet):
     # permission_classes = (AllowAny,)
