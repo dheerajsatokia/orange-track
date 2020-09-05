9586c9c3163332833940464100da0d07a40caefb
Dheeraj Satokia <dheeraj.satokia57@gmail.com>
2020-09-05 16:54:30 +0530

    Correction done in drawings/serializer file

--- drawings/serializer.py
+++ drawings/serializer.py
@@ -18,7 +18,7 @@ class DrawingSerializer(serializers.ModelSerializer):
     class Meta:
         model = Drawing
         fields = '__all__'
-        read_only_field = ('created_at', 'updated_at', 'uploaded_by')
+        read_only_fields = ('created_at', 'updated_at', 'uploaded_by')
 
     def validate(self, data):
         data['uploaded_by_id'] = self.context['request'].user.id
@@ -29,7 +29,7 @@ class DrawingRemarkSerializer(serializers.ModelSerializer):
     class Meta:
         model = DrawingRemark
         fields = '__all__'
-        read_only_field = ('created_at', 'updated_at', 'remarked_by')
+        read_only_fields = ('created_at', 'updated_at', 'remarked_by')
 
     def validate(self, data):
         data['remarked_by_id'] = self.context['request'].user.id
