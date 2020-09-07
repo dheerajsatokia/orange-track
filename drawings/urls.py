39d8ff8329a4ede37d14ac3c980740485feb65dc
Dheeraj Satokia <dheeraj.satokia57@gmail.com>
2020-09-07 14:34:47 +0530

    Urls change for Drawings app

--- drawings/urls.py
+++ drawings/urls.py
@@ -6,9 +6,8 @@ from . import views
 router = routers.SimpleRouter(trailing_slash=False)
 router.register(r'plans', views.PlansViewSet)
 router.register(r'plan-option', views.PlanOptionViewSet)
-router.register(r'drawing', views.DrawingViewSet)
 router.register(r'drawing-remark', views.DrawingRemarkViewSet)
-
+router.register(r'', views.DrawingViewSet)
 urlpatterns = [
 ]
 urlpatterns += format_suffix_patterns(router.urls)
