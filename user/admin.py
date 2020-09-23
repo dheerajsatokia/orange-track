c637d3aa2c8496afc3197fd6355e3be8bfbe9f33
Dheeraj Satokia <dheeraj.satokia57@gmail.com>
2020-09-23 20:03:57 +0530

    settings

--- user/admin.py
+++ user/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
-from .models import User
+from .models import User, Organisation, UserOrganisation
 
 # Register your models here.
 admin.site.site_header = 'Orange Track'
 admin.site.register(User)
+admin.site.register(Organisation)
+admin.site.register(UserOrganisation)
+
