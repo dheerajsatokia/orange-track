3da7ae4b4f8da5c08347a96a405605042c7813f5
Dheeraj Satokia <dheeraj.satokia57@gmail.com>
2020-09-23 19:55:48 +0530

    setting updatedf

--- OrangeTrackBackend/settings.py
+++ OrangeTrackBackend/settings.py
@@ -15,7 +15,7 @@ from datetime import timedelta
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 STATIC_DIR = os.path.join(BASE_DIR, 'static')
 
 # Quick-start development settings - unsuitable for production
