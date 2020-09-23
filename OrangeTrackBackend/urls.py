e29ff06ae39c6055b4a86545675a2a392f25ad96
Dheeraj Satokia <dheeraj.satokia57@gmail.com>
2020-09-23 20:09:46 +0530

    settings

--- OrangeTrackBackend/urls.py
+++ OrangeTrackBackend/urls.py
@@ -15,8 +15,22 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls import url
+from django.contrib import admin
+from django.urls import path, include
+from django.views.static import serve
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/v1/', include('OrangeTrackBackend.api_v1_urls'))
-]
+    path('api/v1/', include('OrangeTrackBackend.api_v1_urls')),
+    url(r'^static/(?P<path>.*)$', serve, {
+        'document_root': settings.STATIC_ROOT,
+    }),
+
+]+ static('/media', document_root=settings.MEDIA_ROOT)
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
